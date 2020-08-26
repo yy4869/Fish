35c29adc6b66dbad3ff72c39948c2b504320f5e1
yy <yaoyuan01@fenbi.com>
2020-08-26 16:42:44 +0800

    ADD: init project

---
+++ Fish/Fish/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Fish
+//
+//  Created by yaoyuan on 2020/8/26.
+//  Copyright © 2020 Tutor. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
